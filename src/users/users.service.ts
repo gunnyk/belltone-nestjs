8ec5d96bcbf1aec13f3b0596a3f1b24540542723
gunnyk <gunnn.kim@gmail.com>
2023-07-25 19:30:18 +0900

    feat: 유저 생성을 위한 함수 추가
    - checkUserExists: 유저 존재 유무 검사
    - saveUser: 유저 디비에 저장
    - sendMemberJoinEmail: 회원 가입 인증 이메일 전송

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
+import * as uuid from 'uuid';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  private checkUserExists(email: string) {
+    return false;
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return;
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {}
+}
