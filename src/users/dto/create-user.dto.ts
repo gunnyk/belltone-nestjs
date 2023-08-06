9e118eb9162afff9cb33859b8db405998dab55b1
gunnyk <gunnn.kim@gmail.com>
2023-08-06 17:10:21 +0900

    feat: name/password 커스텀 유효성 검사

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -5,8 +5,22 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { BadRequestException } from '@nestjs/common';
+import { NotIn } from 'src/utils/decorators/not-in';
 
 export class CreateUserDto {
+  // @Transform(({ value, obj }) => {
+  //   if (obj.password.includes(obj.name.trim())) {
+  //     throw new BadRequestException(
+  //       'password는 name과 같은 문자열을 포함할 수 없습니다.',
+  //     );
+  //   }
+  //   return value.trim();
+  // })
+  @NotIn('password', {
+    message: 'password는 name과 같은 문자열을 포함할 수 없습니다.',
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(30)
